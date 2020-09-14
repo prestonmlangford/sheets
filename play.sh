42280922a573d591e369c0dfbc66b14813a298ee
prestonmlangford <prestonmlangford@gmail.com>
2020-09-14 16:36:39 -0500

    added out directory

--- play.sh
+++ play.sh
@@ -1,2 +1,2 @@
-python main.py mhall.sht sheetmusic/mhall.sco &&
-csound -odac instruments.orc mhall.sco
+python main.py sheetmusic/mhall.sht out/mhall.sco &&
+csound -odac instruments.orc out/mhall.sco
